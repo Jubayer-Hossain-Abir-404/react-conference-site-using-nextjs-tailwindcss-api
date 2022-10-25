0d7c391bc3bf92d4edfe33ca3445a014380f24d3
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-25 10:29:29 +0600

    hero section content positioning

--- components/HeroContent.js
+++ components/HeroContent.js
@@ -5,22 +5,23 @@ const HeroContent = () => {
     const bigHeroImg = `${server}/Images/heroImage2.png`;
     const smallHeroIMg = `${server}/Images/heroImage1.png`;
   return (
-    <div className="heroSection w-9/12 mx-auto mt-[80px] z-20">
+    <div className="heroSection w-11/12 lg:w-9/12 mx-auto mt-[80px] z-20">
       <div className="titleDesc flex flex-col">
-        <div className="">
-          <h1 className="heroTitle text-end">React Conference</h1>
+        <div className="w-9/12">
+          <h1 className="heroTitle text-[48px] lg:text-[140px] z-50 text-end">
+            React Conference
+          </h1>
         </div>
-        <div className="heroDesc mx-auto">
-          <p>
-            Lorem uis diam turpis quam id fermentum.In quis diam turpis quam id
-            fermentum..id fermentum.In quis diam turpis quam id fermentum.
-          </p>
-        </div>
-        <button className="heroButton mx-auto">Buy Tickets</button>
       </div>
-      <div className="flex justify-between">
-        <div className='heroSmallImage relative'>
-            <img src={smallHeroIMg} alt="" />
+      <div className="flex justify-between items-end">
+        <div className="heroSmallImage flex justify-between">
+          <img src={smallHeroIMg} alt="" />
+          <div>
+            <p>
+              Lorem uis diam turpis quam id fermentum.In quis diam turpis quam
+              id fermentum..id fermentum.In quis diam turpis quam id fermentum.
+            </p>
+          </div>
         </div>
         <div className="heroBigImage">
           <img src={bigHeroImg} alt="bigHeroImg" />
