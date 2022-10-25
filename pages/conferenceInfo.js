299ce5761f53edd2361de6d4c8f4dc6066691ce6
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-26 00:41:50 +0600

    conference info init

--- pages/conferenceInfo.js
+++ pages/conferenceInfo.js
@@ -1,9 +1,19 @@
+import Head from 'next/head';
 import React from 'react'
 
 const conferenceInfo = () => {
   return (
-    <div>conferenceInfo</div>
-  )
+    <div className="mt-32  basicContainer conferenceSection">
+      <Head>
+        <title>Conference Info</title>
+      </Head>
+      <h2>Conference info</h2>
+      <p className="conferenceDesc mt-2">
+        Lorem uis diam turpis quam id fermentum.In quis diam turpis quam id
+        fermentum.
+      </p>
+    </div>
+  );
 }
 
 export default conferenceInfo
