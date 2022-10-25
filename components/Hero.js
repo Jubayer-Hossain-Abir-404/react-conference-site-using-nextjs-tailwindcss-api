10bbefde01ab790b803b0801f6585cc7cde9fa60
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-25 17:17:45 +0600

    hero section is somewhat done

--- components/Hero.js
+++ components/Hero.js
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="heroBackgroundImage absolute -z-10">
-      <div className="matrixBackImage lg:block hidden bg-no-repeat">
-        
-      </div>
+    <div className="heroBackgroundImage absolute -z-10 w-[100%] overflow-x-hidden">
+      <div className="matrixBackImage lg:block hidden bg-no-repeat"></div>
     </div>
   );
 }
--- components/HeroContent.js
+++ components/HeroContent.js
@@ -1,25 +1,46 @@
 import Head from 'next/head';
 import React from 'react';
 import { server } from "../config";
-import { config } from "@fortawesome/fontawesome-svg-core";
 
 
 const HeroContent = () => {
     const bigHeroImg = `${server}/Images/heroImage2.png`;
     const smallHeroIMg = `${server}/Images/heroImage1.png`;
+    const blingLine = `${server}/Images/bling-line.png`;
+    const oranamet = `${server}/Images/Ornament.jpg`;
+    const star = `${server}/Images/Star.png`;
+    const vector = `${server}/Images/Vector.png`;
   return (
-    <div className="heroSection w-10/12  mx-auto mt-[80px] z-50">
-      <div className="titleDesc flex flex-col">
+    <div className="heroSection w-10/12  mx-auto mt-[40px] z-50">
+      <div className="heroTitleSection flex flex-col">
         <div className="w-8/12">
+          <img
+            src={blingLine}
+            alt="blingLine"
+            className="-mt-12 lg:-mt-4 left-[11%] lg:left-1/3 absolute"
+          />
+          <img
+            src={vector}
+            alt="vector"
+            className="absolute -z-10 lg:-z-50 left-[28%] lg:left-[40%] mt-8 w-8/12 lg:w-7/12"
+          />
           <h1 className="heroTitle text-[48px] lg:text-[140px] z-50 text-end">
             React Conference
           </h1>
         </div>
       </div>
-      <div className="flex justify-between items-end">
-        <div className="heroSmallImage flex  w-8/12 justify-between">
-          <img src={smallHeroIMg} alt="" />
-          <div className="heroTextButton w-6/12">
+      <div className="flex lg:flex-row flex-col justify-between lg:items-end gap-8">
+        <div className="heroSmallImage flex w-full lg:w-8/12 justify-between">
+          <div className="hidden lg:block">
+            <img src={smallHeroIMg} alt="" className="smallImage" />
+            <img
+              src={oranamet}
+              alt=""
+              className="absolute right-[-4px] lg:right-[64%] -mt-28 -z-20"
+            />
+          </div>
+
+          <div className="heroTextButton w-full lg:w-6/12">
             <p className="whitespace-normal">
               Lorem uis diam turpis quam id fermentum.In quis diam turpis quam
               id fermentum..id fermentum.In quis diam turpis quam id fermentum.
@@ -41,10 +62,25 @@ const HeroContent = () => {
                 ></path>
               </svg>
             </button>
+            <p className="scrollDown hidden lg:block">Scroll Down</p>
           </div>
         </div>
-        <div className="heroBigImage w-4/12">
-          <img src={bigHeroImg} alt="bigHeroImg" />
+        <div className="heroBigImage w-full lg:w-4/12">
+          <img src={bigHeroImg} alt="bigHeroImg" className="bigImage" />
+          <img
+            src={star}
+            alt="star"
+            className="absolute -mt-16 right-2 lg:right-[32%] z-50"
+          />
+        </div>
+
+        <div className="flex justify-between lg:hidden mt-16 ">
+          <img src={smallHeroIMg} alt="" className="smallMobileImage" />
+          <img
+            src={oranamet}
+            alt=""
+            className="mobileOrnament right-0 absolute"
+          />
         </div>
       </div>
     </div>
