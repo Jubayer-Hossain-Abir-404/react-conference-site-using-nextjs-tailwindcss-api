0b85caae50bf0e4e83a58e8330b9ee007a050d53
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-25 21:00:53 +0600

    event schedule header and desc added

--- components/EventSchedule.js
+++ components/EventSchedule.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const EventSchedule = () => {
   return (
-    <>
-      <div className="mt-72 basicContainer mb-32">EventSchedule</div>
-    </>
+      <div className="mt-32 lg:mt-72 basicContainer mb-32 eventScheduleSection">
+        <h2>Event Schedule</h2>
+        <p>
+          Lorem uis diam turpis quam id fermentum.In quis diam turpis quam id
+          fermentum.
+        </p>
+      </div>
   );
 }
 
